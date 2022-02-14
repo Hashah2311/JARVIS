beba5ff680dee201857fffa86bd777bc8c69990c
Hashah <shahharshit063@gmail.com>
2022-02-14 14:59:33 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,5 +1,5 @@
 import os
-os.system("env\python.exe -m pip install speechRecognition datetime wikipedia pypiwin32 requests bs4 psutil pyautogui wheel pyttsx")
+os.system("env\python.exe -m pip install speechRecognition datetime wikipedia pypiwin32 requests bs4 psutil pyautogui wheel")
 whl = "env\python.exe -m pip install PyAudio-0.2.11-cp310-cp310-win_amd64.whl"
 os.system(whl)
 os.system("env\python.exe -m pip install git+https://github.com/jpercent/pyttsx.git")
