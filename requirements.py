b95d490c2754f900590ca9e7b9ae34bbfb7496a4
Hashah2311 <94738391+Hashah2311@users.noreply.github.com>
2022-03-30 06:37:04 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,5 +1,6 @@
 import os
-os.system("env\python.exe -m pip install speechRecognition datetime wikipedia pypiwin32 requests bs4 psutil pyautogui wheel wolframalpha pyfiglet")
+os.system("env\python.exe -m pip install bs4 datetime psutil pyautogui pyfiglet pypiwin32 requests speechRecognition wheel Wikipedia wolframalpha")
 whl = "env\python.exe -m pip install PyAudio-0.2.11-cp310-cp310-win_amd64.whl"
 os.system(whl)
+os.system("env\python.exe -m pip uninstall pyttsx")
 os.system("env\python.exe -m pip install git+https://github.com/jpercent/pyttsx.git")
